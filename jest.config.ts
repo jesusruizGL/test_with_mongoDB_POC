ebacb7c92bd0f29fca9435c6ffb8d3c54d8a99ad
Jesus Ruiz Barreto <jesus.ruiz@gorillalogic.com>
2024-03-10 23:44:55 -0600

    POC

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const jestConfig: JestConfigWithTsJest = {
   // [...]
   // Replace `ts-jest` with the preset you want to use
   // from the above list
-  preset: 'ts-jest',
+  preset: '@shelf/jest-mongodb',
 }
 
 export default jestConfig
--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,6 +1,26 @@
 import {sum} from '../modules/sum'
-
+import {addItem, readItem, testConnection, deleteItem} from '../modules/dbHandler'
 
 test('adds 1 + 2 to equal 3', () =>{
     expect(sum(1,2)).toBe(3)
+})
+
+test('test db connection', async () => {
+    await testConnection();
+})
+
+test('test addItem', async () => {
+    await addItem(1);
+})
+
+test('test readItem', async () => {
+    const itemId = Math.floor(+new Date() / 1000);
+    await addItem(itemId)
+    await readItem(itemId);
+})
+
+test('test deleteItem', async () => {
+    const itemId = Math.floor(+new Date() / 1000)+1;
+    await addItem(itemId)
+    await deleteItem(itemId);
 })
